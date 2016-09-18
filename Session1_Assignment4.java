57766acc288fb75d9a408145883faf7cedcafe2c
arpchauh <arpitchauhan10@gmail.com>
2016-09-18 12:23:40 +0530

    Add files via upload

---
+++ Session1_Assignment4.java
@@ -0,0 +1,27 @@
+package aatika;
+
+public class Session1_Assignment4 {
+
+	public static void main(String[] args) {
+ 
+		int a,b,c,d;
+		int x = 5;
+		int y =10;
+		
+		a= x+y*2;
+		System.out.println("The result of a is : "+a);
+
+		b= x-y+2;
+		System.out.println("The result of b is : "+b);
+		
+		c= (x+y)*2;
+		System.out.println("The result of c is : "+c);
+		
+		d= y%x;
+		System.out.println("The result of d is : "+d);
+
+
+
+	}
+
+}
